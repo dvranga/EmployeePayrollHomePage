eb04c7b27de27844c7aa4ce3ec416eb13c984684
dvranga <dvranga@users.noreply.github.com>
2020-12-07 20:01:58 +0530

    [ranganath]Added:Ability to view employee payroll details in a tabular format from JSON object

--- js/home.js
+++ js/home.js
@@ -13,23 +13,67 @@ const createInnerHtml=()=>{
     <th>Start Date</th>
     <th>Actions</th>
     </tr>`;
-    const innerHtml=`${headerHtml}
+    let innerHtml=`${headerHtml}`;
+    let empPayrollList=createEmployeePayrollJSON();
+    for(const empPayrollData of empPayrollList){
+        innerHtml=`${innerHtml}
     <tr>
-        <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png"></td>
-        <td>Ranganath Devangm</td>
-        <td>Male</td>
-        <td><div class="dept-label">HR</div>
-        <div class="dept-label">Finance</div></td>
-        <td>300000</td>
-        <td>1 Nov 2020</td>
+        <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td>${getDeptHtml(empPayrollData._department)}</td>
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
         <td>
-            <img name="1" onclick="remove(this)" alt="delete"
-            src="../assets/icons/delete-black-18dp.svg">
-            <img name="1" alt="edit" onclick="update(this)"
+            <img name="${empPayrollData._id}" onclick="remove(this)"
+             alt="delete" src="../assets/icons/delete-black-18dp.svg">
+            <img name="${empPayrollData._id}" alt="edit" onclick="update(this)"
             src="../assets/icons/create-black-18dp.svg">
         </td>
     </tr>
     `;
+    }
     document.querySelector('#table-display').innerHTML=innerHtml;
+}
 
+const getDeptHtml=(deptList)=>{
+    let deptHtml='';
+    for(const dept of deptList){
+        deptHtml=`${deptHtml} <div class="dept-label">${dept}</div>`
+    } 
+    return deptHtml;
+}
+
+
+
+const createEmployeePayrollJSON=()=>{
+    let empPayrollListLocal=[
+        {
+            _name:'Ranganath Devangm',
+            _gender:'male',
+            _department:[
+                'Engineering',
+                'Finance'
+            ],
+            _salary:'500000',
+            _startDate:'29 Oct 2019',
+            _note:'',
+            _id:new Date().getTime(),
+            _profilePic:'../assets/profile-images/Ellipse -2.png',
+        },
+        {
+            _name:'Harinath Devangm',
+            _gender:'male',
+            _department:[
+                'Engineering',
+                'Sales'
+            ],
+            _salary:'500000',
+            _startDate:'29 Oct 2019',
+            _note:'',
+            _id:new Date().getTime(),
+            _profilePic:'../assets/profile-images/Ellipse -3.png',
+        }
+    ];
+    return empPayrollListLocal;
 }
